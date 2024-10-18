379f3fb7887f87fae884a651c285cda05dfd3425
karugo-murage <murage.gidion@student.moringaschool.com>
2024-10-18 18:18:57 +0300

    add home page

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+// import React {useState} from "react";
+
+function Home(){
+
+    return(
+        <div>
+            <header>
+                <div>Productivity At Its Best</div>
+                <ul>
+                    <li>My_Tasks</li>
+                    <li>Community</li>
+                </ul>
+            </header>
+        </div>
+    )
+}
+
+export default Home ;
