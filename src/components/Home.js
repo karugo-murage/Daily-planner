795596d951e13a01d513520502e476c5e7dd1c83
karugo-murage <murage.gidion@student.moringaschool.com>
2024-10-18 19:33:47 +0300

    add dummy todos

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,13 @@ import React from "react";
 import '../assets/App.css'; 
 
 function Home() {
+    const tasks = [
+        { id: 1, text: "Hit the gym 💪" },
+        { id: 2, text: "Learn React.js 📚" },
+        { id: 3, text: "Update my portfolio 💻" },
+        { id: 4, text: "Go over coffee with my bestie ☕" },
+        { id: 5, text: "Read the Bible 📖" },
+    ];
     return (
         <>
             <header className="header">
@@ -16,6 +23,17 @@ function Home() {
                 <h2>Welcome to Your Productivity Dashboard!</h2>
                 <p>Here you can manage your tasks efficiently and connect with your community.</p>
             </div>
+            <div className="task-list">
+                    {tasks.map((task) => (
+                        <div key={task.id} className="task-item">
+                            <span className="task-text">{task.text}</span>
+                            <div className="task-buttons">
+                                <button className="edit-button">Edit</button>
+                                <button className="complete-button">Completed</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
         </>
     );
 }
